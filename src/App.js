835210354563648615ca442e713db39cf4b419d5
Karan Janthe <86996507+KMJ-007@users.noreply.github.com>
2022-02-02 16:58:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import jsPDF from 'jspdf';
 
-function App() {
+import React, { Component } from 'react';
+
+export default class App extends Component {
+   
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.pdfMaker=this.pdfMaker.bind(this);
+  }
+  pdfMaker = () =>{
+    var doc = new jsPDF('portrait','px','a4','false');
+    
+    // console.log(this.state.value);.
+    
+    doc.text(50,40,this.state.value);
+    doc.save("test.pdf");
+  };
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    hello <br/>
+    <form >
+        <label>
+          name
+          <input type="name" className="content"value={this.state.value} onChange={this.handleChange} />
+        </label>
+      </form>
+    <button onClick={this.pdfMaker}>Download pdf</button>
     </div>
   );
 }
 
-export default App;
+}
+ 
